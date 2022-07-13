aeb7907732cc42825b7541a722e17fe926ad0952
Leedosuyeon <leedosuyeon@gmail.com>
2022-07-13 16:58:45 +0900

    github 업로드 연습

---
+++ src/com/KoreaIT/java/BAm/Main.java
@@ -0,0 +1,11 @@
+package com.KoreaIT.java.BAm;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("=====프로긂 시작");
+		System.out.println("입력된 명령어: %s/n");
+	}
+
+}
