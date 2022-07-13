70505ccb9f865bd4dab9a5e53997b4575de4533f
Leedosuyeon <leedosuyeon@gmail.com>
2022-07-13 18:29:27 +0900

    게시물 작성

--- src/com/KoreaIT/java/BAm/Main.java
+++ src/com/KoreaIT/java/BAm/Main.java
@@ -1,11 +1,26 @@
 package com.KoreaIT.java.BAm;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("=====프로긂 시작");
+		System.out.println("=====프로그램 시작");
+		
+		Scanner sc = new Scanner(System.in);
+		 
+		while (true ) {
+			System.out.println("명령어 )");
+			String cmd = sc.nextLine();
+			
+			if(cmd.equals("exit")) {
+				break;
+			}
+		}
 		System.out.println("입력된 명령어: %s/n");
+		sc.close();
+		
 	}
 
 }
